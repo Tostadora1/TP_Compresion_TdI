bcc7e3df9219effbbe80083c4f000bf836cf22f1
Fernando <Fernando@DESKTOP-01G8JTV>
2019-10-14 20:00:41 -0300

    Table generator integrado a IOFile.java

--- modelo/IOFile.java
+++ modelo/IOFile.java
@@ -16,6 +16,8 @@ import java.io.ObjectOutputStream;
 import java.io.UnsupportedEncodingException;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class IOFile
 {
@@ -117,5 +119,38 @@ public class IOFile
         {
         }
         return st.toString();
+    }   
+
+    public static HashMap<Character, Double> generator(String filename)
+        throws FileNotFoundException
+    {
+        HashMap<Character, Double> table = new HashMap<Character, Double>();
+        int total = 0;
+        try
+        {
+            BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(filename), "utf-8"));
+            int control = in.read();
+            char current = (char) control;
+            while (control != -1)
+            {
+                total++;                
+                if (table.containsKey(current))
+                    table.put(current, table.get(current) + 1.0);
+                else
+                    table.put(current, 1.0);
+                control = in.read();
+                current = (char) control;
+            }
+        }
+        catch (IOException e)
+        {
+            System.out.println("No se encontro el archivo");
+        }
+        for (Map.Entry<Character, Double> entry: table.entrySet())
+        {
+            table.put(entry.getKey(), entry.getValue() / total);
+        }
+        return table;
     }
+    
 }
